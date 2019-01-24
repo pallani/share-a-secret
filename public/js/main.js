5fe15ae1c954fa2f49b91fe1dfcab391e7ed3d8d
Pallani Kumaran <pallani@data.gov.sg>
2019-01-24 19:19:29 +0800

    Add basic ui and generation of webrtc offer/answer

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,47 @@
 function init () {
+  console.log('window.location.hash:', window.location.hash, window.location.hash === '')
+
+  let shareNowButton = $('#shareNowButton')
+  let welcomeJumboTron = $('#welcomeJumboTron')
+  let mainShareContainer = $('#mainShareContainer')
+  let secretField = $('#secretField')
+  let showHideButton = $('#showHideButton')
+
+  if (window.location.hash === '') {
+    welcomeJumboTron.removeClass('d-none')
+  } else {
+    let offer = window.atob(window.location.hash.substring(1))
+    var p2 = new window.SimplePeer({ initiator: false, trickle: false })
+    p2.on('signal', (data) => {
+      console.log('answer:', data)
+    })
+    p2.signal(offer)
+  }
+
+  shareNowButton.on('click', () => {
+    welcomeJumboTron.addClass('d-none')
+    mainShareContainer.removeClass('d-none')
+
+    if (window.location.hash === '') {
+      var p = new window.SimplePeer({ initiator: true, trickle: false })
+      p.on('error', (err) => console.log('error', err))
+      p.on('signal', (data) => {
+        console.log('offer:', data)
+        window.location.hash = window.btoa(JSON.stringify(data))
+      })
+    }
+  })
+
+  showHideButton.on('click', (e) => {
+    if (secretField.attr('type') === 'password') {
+      secretField.attr('type', 'text')
+    } else {
+      secretField.attr('type', 'password')
+    }
+  })
+}
+function init2 () {
+  console.log('window.location.hash:', window.location.hash, window.location.hash === '')
   var p = new window.SimplePeer({ initiator: window.location.hash === '#1', trickle: false })
 
   p.on('error', (err) => console.log('error', err))

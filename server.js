778186a17f2952e950099f7e0d73825f4d1c9a25
Pallani Kumaran <pallanikumaran@gmail.com>
2019-01-30 01:01:17 +0800

    Add comments to node server file

--- server.js
+++ server.js
@@ -6,37 +6,18 @@ const server = require('http').Server(app)
 // Socket.io
 const io = require('socket.io')(server)
 
+// Socket.io is used for signalling before the webrtc connection is set-up
 io.on('connection', (socket) => {
+  // Respond to client request to join a room
   socket.on('join', room => {
     socket.join(room)
   })
 
+  // Respond to client request to send webrtc answer to room
   socket.on('answer', (data) => {
     io.to(data.room).emit('answer', data.answer)
   })
 })
-// const p2p = require('socket.io-p2p-server').Server
-// io.use(p2p)
-
-// io.on('connection', (socket) => {
-//   console.log('socket id:', socket.id)
-// })
-
-// io.on('connection', function (socket) {
-//   socket.on('peer-msg', function (data) {
-//     console.log('Message from peer: %s', data)
-//     socket.broadcast.emit('peer-msg', data)
-//   })
-
-//   socket.on('peer-file', function (data) {
-//     console.log('File from peer: %s', data)
-//     socket.broadcast.emit('peer-file', data)
-//   })
-
-//   socket.on('go-private', function (data) {
-//     socket.broadcast.emit('go-private', data)
-//   })
-// })
 
 const serverPort = process.env.PORT || 10000
 
